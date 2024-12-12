ed084b46a5563ae0e3d333fad03d88ccf719ac02
Ashish Gowtham Vajjala <105256502+ashishnex007@users.noreply.github.com>
2024-12-12 14:25:54 +0530

    Update connectDB.js

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,20 +1,17 @@
 const mongoose = require('mongoose');
-
 require('dotenv').config();
 
-const connectDB = () => {
-    const dbConnectionString = process.env.MONGO_URI;
-    
-    mongoose.connect(dbConnectionString, {
-        useUnifiedTopology: true,
-        useNewUrlParser: true
-    });
-    
-    const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-    db.once('open', () => {
-        console.log('Connected to MongoDB');
-    });
-}
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected successfully');
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        process.exit(1);
+    }
+};
 
 module.exports = connectDB;
